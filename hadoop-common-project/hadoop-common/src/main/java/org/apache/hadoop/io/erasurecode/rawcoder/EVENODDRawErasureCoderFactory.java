396ad242866fea738dbdd5b39fd195db0437fcf2
Li Fengchen <384965109@qq.com>
2019-05-24 23:20:04 +0800

    EVENODD+bug全部修复

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/rawcoder/EVENODDRawErasureCoderFactory.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/rawcoder/EVENODDRawErasureCoderFactory.java
@@ -14,14 +14,12 @@ public class EVENODDRawErasureCoderFactory implements RawErasureCoderFactory {
 
     @Override
     public RawErasureEncoder createEncoder(ErasureCoderOptions coderOptions) {
-        return new RSRawEncoder(coderOptions);
-//        return new EVENODDRawEncoder(coderOptions);
+        return new EVENODDRawEncoder(coderOptions);
     }
 
     @Override
     public RawErasureDecoder createDecoder(ErasureCoderOptions coderOptions) {
-        return new RSRawDecoder(coderOptions);
-//        return new EVENODDRawDecoder(coderOptions);
+        return new EVENODDRawDecoder(coderOptions);
     }
 
     @Override
